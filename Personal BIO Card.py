5c14f0d589faf5a8aad714b6e6385678eea18fa5
Rajit-Sachan <rajitsachan@gmail.com>
2026-02-24 16:01:28 +0530

    Add files via upload

---
+++ Personal BIO Card.py
@@ -0,0 +1,17 @@
+#This is my bio card progra
+#Personal details
+name = "Rajit Sachan"
+age = 22
+course = "CSE"
+college = "CMRIT"
+email = "rajitsachan@gmail.com"
+
+print("     ╔════════════════════════════════════╗")
+print("     ║           STUDENT PROFILE          ║")
+print("     ╠════════════════════════════════════╣")
+print(f"     ║ Name    : {name}             ║")
+print(f"     ║ Age     : {age}                       ║")
+print(f"     ║ Course  : {course}                      ║")
+print(f"     ║ College : {college}                    ║")
+print(f"     ║ Email   : {email}    ║")
+print("     ╚════════════════════════════════════╝")
